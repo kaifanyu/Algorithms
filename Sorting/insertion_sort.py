f1b31973e51d16411e2260bd653f0546c9d9b6e2
kaifanyu <kaifany1@uci.edu>
2024-10-01 12:10:53 -0700

    upload

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,6 @@
+# Example file: insertion_sort.py
+
+# Each sorting function should accept a list of integers as the single required
+# parameter, as shown below. The input list should be sorted upon completion.
+def insertion_sort(nums: list[int]):
+	pass
